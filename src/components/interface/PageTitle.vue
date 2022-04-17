b53c36643533fc94a9d63c5dfe67e840bb11ef06
Andrii Kotliar <andrijkotliar@gmail.com>
2022-04-17 18:44:39 +0300

    Add page title component

---
+++ src/components/interface/PageTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="page-title">
+    {{title}} <span>{{value}}</span>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'PageTitle',
+  props: {
+    title: String,
+    value: String
+  }
+}
+</script>
+
+<style>
+
+</style>
